0574d034d88481a59fce6432c34e2e8aa91395e4
yintianyu <eecsyty@outlook.com>
2020-10-14 10:05:13 +0800

    update readme.md and project name

--- build.sbt
+++ build.sbt
@@ -26,7 +26,7 @@ def javacOptionsVersion(scalaVersion: String): Seq[String] = {
   }
 }
 
-name := "chisel-module-template"
+name := "hpme"
 
 version := "3.3.0"
 
--- src/main/scala/hpme.scala
+++ src/main/scala/hpme.scala
@@ -1,3 +1,15 @@
+package hpme
+
+import chisel3._
+import chisel3.util._
+import freechips.rocketchip.subsystem.BaseSubsystem
+import freechips.rocketchip.config.{Field, Parameters}
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.regmapper.{HasRegMap, RegField}
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.util._
+import testchipip.{StreamIO, StreamChannel, TLHelper}
+
 class hpme {
 
 }
